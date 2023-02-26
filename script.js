6fc9480ace8fe51d583ef219c0a27290aa03fd17
Sakshi <sakshiarya41@gmail.com>
2023-02-26 20:47:33 +0530

    adjusted board dimensions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Take input from user
 var size = parseInt(
     window.prompt(
-        "Welcome, to N-Queen Puzzle Game. Please, Enter size of Board:4 or 8"
+        "Welcome, to N-Queen Puzzle Game. Please, Enter size of Board: 4 or 8"
     )
 );
 
@@ -9,7 +9,7 @@ var size = parseInt(
 
 while (!(size == 4 || size== 8)) {
     alert("Not Valid Board size");
-    var size = parseInt(window.prompt("Enter size of grid :4 or 8"));
+    var size = parseInt(window.prompt("Enter size of board: 4 or 8"));
 }
 
 var id = 0;
@@ -43,93 +43,96 @@ chessboard.style.border="4px solid black";
 // On clicking cell
 
 function placeq(event) {
-    var id = event.target.id;
-    var q = eval(id);
-    var r = Math.floor(q / size);
-    var c = q % size;
-    var queen=document.getElementById(q);
+    if(count<size)
+    {
+        var id = event.target.id;
+        var q = eval(id);
+        var r = Math.floor(q / size);
+        var c = q % size;
+        var queen=document.getElementById(q);
 
-    //Check if clicked cell already contains queen
+        //Check if clicked cell already contains queen
 
-    if (queen.classList.contains("queen")){
-        queen.style.backgroundImage= "none";
-        queen.classList.remove("queen");
-        queen.classList.remove("extra");
-        queen.classList.remove("occupied");
-        count--;
+        if (queen.classList.contains("queen")){
+            queen.style.backgroundImage= "none";
+            queen.classList.remove("queen");
+            queen.classList.remove("extra");
+            queen.classList.remove("occupied");
+            count--;
 
-        // For removing classlist
+            // For removing classlist
 
-         //For checking classlist Horizontally
+            //For checking classlist Horizontally
 
-        for (var h = r * size; h < r * size + size; h++) {
-            var dot=document.getElementById(h);
-            if(!(dot.classList.contains("extra"))){
-                dot.classList.remove("occupied");
-            }
-            else{
-                dot.classList.remove("extra");
-            }
-            
-        }
-
-        // For checking classlist Vertically
-
-        for (var v = c; v < size * size; v = v + size) {
-            var dot=document.getElementById(v);
-            if(!(dot.classList.contains("extra"))){
-                dot.classList.remove("occupied");
-            }
-            else{
-                dot.classList.remove("extra");
-            }
-        }
-
-       // For checking classlist Diagonally on one way   
-                                    
-        if (c > r) {
-            var x1 = c - r;
-            var x2 = q + (size + 1) * (size - c);
-        } else {
-            var x1 = (r - c) * size;
-            var x2 = q + (size + 1) * (size - r);
-        }
-        for (var x = x1; x < x2; x = x + size + 1) {
-            var dot=document.getElementById(x);
-            if(!(dot.classList.contains("extra"))){  
-                dot.classList.remove("occupied");
-            }
-            else{
-                dot.classList.remove("extra");
-            }
+            for (var h = r * size; h < r * size + size; h++) {
+                var dot=document.getElementById(h);
+                if(!(dot.classList.contains("extra"))){
+                    dot.classList.remove("occupied");
+                }
+                else{
+                    dot.classList.remove("extra");
+                }
                 
-        }
-
-        // For checking classlist Diagonally on second way
-
-        if (r + c > size - 1) {
-            var y1 = q - (size - 1 - c) * (size - 1);
-            var y2 = q + (size - 1) * (size - r);
-        } else {
-            var y1 = r + c;
-            var y2 = q + size * c;
-        }
-        for (var y = y1; y < y2; y = y + (size - 1)) {
-            var dot=document.getElementById(y);
-            if(!(dot.classList.contains("extra"))){
-                dot.classList.remove("occupied");
             }
-            else{
-                dot.classList.remove("extra");
+
+            // For checking classlist Vertically
+
+            for (var v = c; v < size * size; v = v + size) {
+                var dot=document.getElementById(v);
+                if(!(dot.classList.contains("extra"))){
+                    dot.classList.remove("occupied");
+                }
+                else{
+                    dot.classList.remove("extra");
+                }
             }
-        }
-    }  // if not contains queen then place queen
-    
-    else {
-        if(count<size)
+
+        // For checking classlist Diagonally on one way   
+                                        
+            if (c > r) {
+                var x1 = c - r;
+                var x2 = q + (size + 1) * (size - c);
+            } else {
+                var x1 = (r - c) * size;
+                var x2 = q + (size + 1) * (size - r);
+            }
+            for (var x = x1; x < x2; x = x + size + 1) {
+                var dot=document.getElementById(x);
+                if(!(dot.classList.contains("extra"))){  
+                    dot.classList.remove("occupied");
+                }
+                else{
+                    dot.classList.remove("extra");
+                }
+                    
+            }
+
+            // For checking classlist Diagonally on second way
+
+            if (r + c > size - 1) {
+                var y1 = q - (size - 1 - c) * (size - 1);
+                var y2 = q + (size - 1) * (size - r);
+            } else {
+                var y1 = r + c;
+                var y2 = q + size * c;
+            }
+            for (var y = y1; y < y2; y = y + (size - 1)) {
+                var dot=document.getElementById(y);
+                if(!(dot.classList.contains("extra"))){
+                    dot.classList.remove("occupied");
+                }
+                else{
+                    dot.classList.remove("extra");
+                }
+            }
+        }  // if not contains queen then place queen
+        
+        else 
         {
             // check whether queen position is safe or not
-           if (safe(q)) { 
+            
+            if (safe(q))
+            { 
 
                 //check Horizontal relative cells for  Queen
                     
@@ -181,22 +184,22 @@ function placeq(event) {
 
                 // For checking Diagonally for any Queen on second way
 
-                    if (r + c > size - 1) {
-                        var y1 = q - (size - 1 - c) * (size - 1);
-                        var y2 = q + (size - 1) * (size - r);
-                    } else {
-                        var y1 = r + c;
-                        var y2 = q + size * c;
+                if (r + c > size - 1) {
+                    var y1 = q - (size - 1 - c) * (size - 1);
+                    var y2 = q + (size - 1) * (size - r);
+                } else {
+                    var y1 = r + c;
+                    var y2 = q + size * c;
+                }
+                for (var y = y1; y < y2; y = y + (size - 1)) {
+                    var dot=document.getElementById(y);
+                    if(dot.classList.contains("occupied")){
+                        dot.classList.add("extra");
                     }
-                    for (var y = y1; y < y2; y = y + (size - 1)) {
-                        var dot=document.getElementById(y);
-                        if(dot.classList.contains("occupied")){
-                            dot.classList.add("extra");
-                        }
-                        else{
-                            dot.classList.add("occupied");
-                        }
+                    else{
+                        dot.classList.add("occupied");
                     }
+                }
 
                 //insert queen
                 queen.style.backgroundImage="url('Images/queen.png')";
@@ -206,15 +209,15 @@ function placeq(event) {
                 queen.classList.add("queen");
                 count++; 
                 if (count == size) {
-                    alert("Hurray🎉🎉, you solved it!");
+                    alert("Hurray..!!🎉🎉, you solved it!");
                 }
 
             }                   
         }
-        else{
-            alert("Click on Reset Game to play again.!!")
-        }
                     
+    }
+    else{
+        alert("Click on Reset Game to play again.!!")
     }
 }
 
@@ -224,7 +227,7 @@ function safe(a) {
     var occuppied=document.getElementById(a);
     var clsList=occuppied.classList;
     if ( clsList.contains("occupied")) {
-        alert('position  not possible');
+        alert('Position not possible');
         return false;   
     } else {
         return true;
